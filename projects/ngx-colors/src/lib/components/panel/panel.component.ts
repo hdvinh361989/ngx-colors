544b743479444a3ba797fc7e1c3f67f839164e42
Vinh Hoang <vinh@streethawk.com>
2022-05-03 13:23:03 +0700

    update: panel layout

--- projects/ngx-colors/src/lib/components/panel/panel.component.ts
+++ projects/ngx-colors/src/lib/components/panel/panel.component.ts
@@ -19,14 +19,14 @@ import {
   animate,
   keyframes,
 } from "@angular/animations";
-import { isDescendantOrSame } from "../../helpers/helpers";
-import { ColorFormats } from "../../enums/formats";
-import { ConverterService } from "../../services/converter.service";
-import { defaultColors } from "../../helpers/default-colors";
-import { formats } from "../../helpers/formats";
-import { NgxColorsTriggerDirective } from "../../directives/ngx-colors-trigger.directive";
-import { Hsva } from "../../clases/formats";
-import { NgxColor } from "../../clases/color";
+import {isDescendantOrSame} from "../../helpers/helpers";
+import {ColorFormats} from "../../enums/formats";
+import {ConverterService} from "../../services/converter.service";
+import {defaultColors} from "../../helpers/default-colors";
+import {formats} from "../../helpers/formats";
+import {NgxColorsTriggerDirective} from "../../directives/ngx-colors-trigger.directive";
+import {Hsva} from "../../clases/formats";
+import {NgxColor} from "../../clases/color";
 
 @Component({
   selector: "ngx-colors-panel",
@@ -36,7 +36,7 @@ import { NgxColor } from "../../clases/color";
     trigger("colorsAnimation", [
       transition("void => slide-in", [
         // Initially all colors are hidden
-        query(":enter", style({ opacity: 0 }), { optional: true }),
+        query(":enter", style({opacity: 0}), {optional: true}),
         //slide-in animation
         query(
           ":enter",
@@ -44,22 +44,22 @@ import { NgxColor } from "../../clases/color";
             animate(
               ".3s ease-in",
               keyframes([
-                style({ opacity: 0, transform: "translatex(-50%)", offset: 0 }),
+                style({opacity: 0, transform: "translatex(-50%)", offset: 0}),
                 style({
                   opacity: 0.5,
                   transform: "translatex(-10px) scale(1.1)",
                   offset: 0.3,
                 }),
-                style({ opacity: 1, transform: "translatex(0)", offset: 1 }),
+                style({opacity: 1, transform: "translatex(0)", offset: 1}),
               ])
             ),
           ]),
-          { optional: true }
+          {optional: true}
         ),
       ]),
       //popup animation
       transition("void => popup", [
-        query(":enter", style({ opacity: 0, transform: "scale(0)" }), {
+        query(":enter", style({opacity: 0, transform: "scale(0)"}), {
           optional: true,
         }),
         query(
@@ -68,13 +68,13 @@ import { NgxColor } from "../../clases/color";
             animate(
               "500ms ease-out",
               keyframes([
-                style({ opacity: 0.5, transform: "scale(.5)", offset: 0.3 }),
-                style({ opacity: 1, transform: "scale(1.1)", offset: 0.8 }),
-                style({ opacity: 1, transform: "scale(1)", offset: 1 }),
+                style({opacity: 0.5, transform: "scale(.5)", offset: 0.3}),
+                style({opacity: 1, transform: "scale(1.1)", offset: 0.8}),
+                style({opacity: 1, transform: "scale(1)", offset: 1}),
               ])
             ),
           ]),
-          { optional: true }
+          {optional: true}
         ),
       ]),
     ]),
@@ -84,14 +84,15 @@ export class PanelComponent implements OnInit {
   @HostListener("document:mousedown", ["$event"])
   click(event) {
     if (this.isOutside(event)) {
-      this.emitClose("cancel");
+      // this.emitClose("cancel");
     }
   }
 
-  @HostListener("document:scroll")
-  onScroll() {
+  @HostListener("document:scroll", ['$event'])
+  onScroll(event) {
     this.onScreenMovement();
   }
+
   @HostListener("window:resize")
   onResize() {
     this.onScreenMovement();
@@ -100,12 +101,15 @@ export class PanelComponent implements OnInit {
   @HostBinding("style.top.px") public top: number;
   @HostBinding("style.left.px") public left: number;
   @ViewChild("dialog") panelRef: ElementRef;
+
   constructor(
     public service: ConverterService,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) {
+  }
 
-  public color = "#000000";
+  color = "#000000";
+
   public previewColor: string = "#000000";
   public hsva = new Hsva(0, 1, 1, 1);
 
@@ -139,6 +143,7 @@ export class PanelComponent implements OnInit {
     this.hsva = this.service.stringToHsva(this.color);
     this.indexSeleccionado = this.findIndexSelectedColor(this.palette);
   }
+
   public ngAfterViewInit() {
     this.setPositionY();
   }
@@ -202,7 +207,7 @@ export class PanelComponent implements OnInit {
         if (
           this.service.getFormatByString(this.color) != format.toLowerCase()
         ) {
-          this.setColor(this.service.stringToHsva(this.color));
+          this.setColorFromHsva(this.service.stringToHsva(this.color));
         }
       } else {
         console.error("Format provided is invalid, using HEX");
@@ -276,9 +281,9 @@ export class PanelComponent implements OnInit {
 
   public getBackgroundColor(color) {
     if (typeof color == "string") {
-      return { background: color };
+      return {background: color};
     } else {
-      return { background: color?.preview };
+      return {background: color?.preview};
     }
   }
 
@@ -290,16 +295,16 @@ export class PanelComponent implements OnInit {
     alpha,
     colors: Array<string | NgxColor>
   ): Array<any> {
-    var result = [];
+    const result = [];
     for (let i = 0; i < colors.length; i++) {
       const color = colors[i];
       if (typeof color == "string") {
-        let newColor = this.service.stringToHsva(color);
+        const newColor = this.service.stringToHsva(color);
         newColor.onAlphaChange(alpha);
         result.push(this.service.toFormat(newColor, this.format));
       } else {
-        let newColor = new NgxColor();
-        let newColorPreview = this.service.stringToHsva(color.preview);
+        const newColor = new NgxColor();
+        const newColorPreview = this.service.stringToHsva(color.preview);
         newColorPreview.onAlphaChange(alpha);
         newColor.preview = this.service.toFormat(newColorPreview, this.format);
         newColor.variants = this.ChangeAlphaOnPalette(alpha, color.variants);
@@ -314,71 +319,59 @@ export class PanelComponent implements OnInit {
    * @param string color
    */
   public changeColor(color: string): void {
-    this.setColor(this.service.stringToHsva(color));
-    // this.triggerInstance.onChange();
-    // this.emitClose("accept");
+    this.setColorFromString(color);
     this.menu = 1;
   }
 
   public onChangeColorPicker(event: Hsva) {
     this.temporalColor = event;
     this.color = this.service.toFormat(event, this.format);
-    // this.setColor(event);
     this.triggerInstance.sliderChange(
       this.service.toFormat(event, this.format)
     );
   }
 
   public changeColorManual(color: string): void {
-    this.previewColor = color;
-    this.color = color;
-    this.hsva = this.service.stringToHsva(color);
-    this.triggerInstance.setColor(this.color);
-    // this.triggerInstance.onChange();
+    this.setColorFromString(color);
   }
 
-  setColor(value: Hsva) {
+  setColorFromHsva(value: Hsva) {
     this.hsva = value;
     this.color = this.service.toFormat(value, this.format);
-    this.setPreviewColor(value);
-    this.triggerInstance.setColor(this.color);
-  }
-
-  setPreviewColor(value: Hsva) {
     this.previewColor = this.service.hsvaToRgba(value).toString();
   }
-  hsvaToRgba;
-  onChange() {
-    // this.triggerInstance.onChange();
+
+  setColorFromString(color: string) {
+    this.hsva = this.service.stringToHsva(color);
+    this.color = color;
+    this.previewColor = color;
   }
 
   public onColorClick(color) {
-    this.variants = color.variants;
-    this.menu = 2;
-  }
-
-  public addColor() {
-    this.menu = 3;
-    this.backupColor = this.color;
-    this.color = this.color || "#FF0000";
-    this.temporalColor = this.service.stringToHsva(this.color);
+    if (typeof color === 'string') {
+      this.changeColor(color);
+    } else {
+      this.variants = color.variants;
+      this.menu = 2;
+    }
   }
 
   public nextFormat() {
     if (this.canChangeFormat) {
       this.format = (this.format + 1) % this.colorFormats.length;
-      this.setColor(this.hsva);
+      this.setColorFromHsva(this.hsva);
     }
   }
 
-  public emitClose(status: "cancel" | "accept") {
-    if (this.menu == 3) {
-      if (status == "cancel") {
-      } else if (status == "accept") {
-        this.setColor(this.temporalColor);
-      }
-    }
+  close(){
     this.triggerInstance.close();
+
+  }
+
+  accept(){
+    this.triggerInstance.setColor(this.color);
+    this.triggerInstance.close();
+
   }
 
   public onClickBack() {
@@ -392,5 +385,10 @@ export class PanelComponent implements OnInit {
 
   isOutside(event) {
     return event.target.classList.contains("ngx-colors-overlay");
+  }
+
+  onOpenedScroll($event){
+    console.log('on scroll');
+    $event.stopPropagation();
   }
 }

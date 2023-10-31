9c9751b12d532b8bb5c8081e3251d5386dc89886
Kingsley <kingsley@nustechnology.com>
2023-10-31 09:00:12 +0700

    update: use built-in function to convert colors

--- projects/ngx-colors/src/lib/components/panel/panel.component.ts
+++ projects/ngx-colors/src/lib/components/panel/panel.component.ts
@@ -23,8 +23,7 @@ import { formats } from "../../helpers/formats";
 import { NgxColorsTriggerDirective } from "../../directives/ngx-colors-trigger.directive";
 import { Hsva } from "../../clases/formats";
 import { NgxColor } from "../../clases/color";
-import { COLOR_REGEX } from "../../constants/contants";
-import { rgbaToHex } from "../../helpers/helpers";
+import { HEX_REGEX, RGBA_REGEX } from "../../constants/contants";
 
 @Component({
   selector: "ngx-colors-panel",
@@ -367,7 +366,7 @@ export class PanelComponent implements OnInit {
   accept() {
     if (!this.disabled) {
       this.triggerInstance.setColor(
-        this.color.startsWith("#") ? this.color : rgbaToHex(this.color)
+        this.service.stringToFormat(this.color, ColorFormats.HEX)
       );
       this.triggerInstance.close();
     }
@@ -391,6 +390,8 @@ export class PanelComponent implements OnInit {
   }
 
   get disabled(): boolean {
-    return !COLOR_REGEX.test(this.color.toLowerCase());
+    return (
+      !RGBA_REGEX.test(this.color.toLowerCase()) && !HEX_REGEX.test(this.color)
+    );
   }
 }
--- projects/ngx-colors/src/lib/helpers/helpers.ts
+++ projects/ngx-colors/src/lib/helpers/helpers.ts
@@ -26,28 +26,3 @@ export function getFormat(format: string): ColorFormats {
   }
   return result;
 }
-
-export function rgbaToHex(rgba: string) {
-  const values = rgba.split(/,|\(|\)/);
-  const r = +values[1],
-    g = +values[2],
-    b = +values[3];
-
-  const a = values[4] === "" ? 1 : +values[4];
-  let outParts = [
-    r.toString(16),
-    g.toString(16),
-    b.toString(16),
-    Math.round(a * 255)
-      .toString(16)
-      .substring(0, 2),
-  ];
-
-  outParts.forEach(function (part, i) {
-    if (part.length === 1) {
-      outParts[i] = "0" + part;
-    }
-  });
-
-  return "#" + outParts.join("");
-}
